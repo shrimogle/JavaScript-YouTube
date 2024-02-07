08f6766f6b6233e1d999996ac904c2558fbefd32
shrimogle <104825622+shrimogle@users.noreply.github.com>
2024-02-07 13:39:08 +0000

    Variables

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // Treat all JS code as newer version of JS
+
+// alert(3+3)   // We are using nodejs not  browser
+
+console.log(3+3); 
+console.log("Shrinivas")
+
+console.log(7
+    +
+    8)//  Code readability should be high 
+
+
+let string = "Shrinivas"
+let num = 14
+let boolean = true
+
+// Number  =>  range 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value 
+// undefine => not assigned
+// symbol => unique 
+
+
+// object =>
+
+console.log(typeof "Shrinivas")
+
+console.log(typeof num)
+
+console.log(typeof null)  //object
+
+console.log(typeof undefined) //undefined
