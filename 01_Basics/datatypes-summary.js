5d8d1f40944bd84a1a119664aecd2f4836f7c1ad
shrimogle <104825622+shrimogle@users.noreply.github.com>
2024-04-12 13:35:09 +0000

    Memory

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -40,4 +40,29 @@ console.log(typeof myFunction);
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
-// Stack(Primitive)   , Heap(Non-Primitive)
+// Stack Memory(Primitive)   ,
+// Heap Memory(Non-Primitive)
+
+let myname = "Shrinivas"
+
+let anothername = myname
+anothername= "Rushi"
+
+console.log(myname);
+console.log(anothername);
+
+/***************      *************** */
+
+let myid = {
+    PRN:"RBT21CB014",
+    email:"mogleshri@gmail.com"
+}
+
+let anotherid = myid
+
+anotherid.PRN = "RBT21CB027"
+anotherid.email = "shrimogle@gmail.com"
+
+console.log(anotherid.PRN,anotherid.email);
+
+console.log(myid.PRN,myid.email);
