7171f434b44202b3e8b780f9a6cb916e6726234e
shrimogle <104825622+shrimogle@users.noreply.github.com>
2024-04-14 07:35:23 +0000

    Objects

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -20,19 +20,19 @@ const JsUser = {
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+ console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+ //Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+ //console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
+ JsUser.greeting = function(){
+   console.log("Hello JS user");
+ }
+ JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
-}
+ }
 
-console.log(JsUser.greeting());
+ console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
